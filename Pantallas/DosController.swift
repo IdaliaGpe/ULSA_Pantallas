fd23b6cb15398041cfec9904edc745cab12b77bf
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2022-09-19 18:41:08 -0700

    cosas

---
+++ Pantallas/DosController.swift
@@ -0,0 +1,18 @@
+//
+//  DosController.swift
+//  Pantallas
+//
+//  Created by Alumno on 9/19/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class DosController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func doTapVolver(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Pantallas/TresController.swift
@@ -0,0 +1,15 @@
+//
+//  TresController.swift
+//  Pantallas
+//
+//  Created by Alumno on 9/19/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class TresController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
